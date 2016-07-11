19087df0e018ff2ec737e60fc7d8c10b9ec7897c
crazynoodle <crazynoodle@live.com>
2016-07-11 14:40:33 +0800

    Create a-1.cpp

---
+++ a-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.ends());
+        //vector<int>::iterator n_iter;
+        int a = 0;
+        int b = 0;
+        int i = 0;
+        vector<int> res;
+        //for(int i =0;i<nums.size();i++){
+        do{
+            // if(a+b == target) break;
+            a = nums[i];
+            for(int j = i+1;j<nums.size();j++){
+                if(a + nums[j] == target){
+                    b = nums[j];
+                    res.push_back(i);
+                    res.push_back(j);
+                    break;   
+                }else if(a+nums[j] > target){
+                    b = nums[j];
+                    break;
+                }else continue;
+            }
+            i++;
+        }while(a+b != target && i <nums.size())
+    }
+};
