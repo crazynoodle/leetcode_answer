4b26136d3e5d3b68c5118c4aefba11a73951777e
crazynoodle <crazynoodle@live.com>
2016-07-12 16:15:50 +0800

    Create RegularExpressionMatch.cpp

---
+++ RegularExpressionMatch.cpp
@@ -0,0 +1,37 @@
+/*
+ *  '.' Matches any single character.
+ *  '*' Matches zero or more of the preceding element.
+ *  
+ *  The matching should cover the entire input string (not partial).
+ *  
+ *  The function prototype should be:
+ *  bool isMatch(const char *s, const char *p)
+ *  
+ *  Some examples:
+ *  isMatch("aa","a") → false
+ *  isMatch("aa","aa") → true
+ *  isMatch("aaa","aa") → false
+ *  isMatch("aa", "a*") → true
+ *  isMatch("aa", ".*") → true
+ *  isMatch("ab", ".*") → true
+ *  isMatch("aab", "c*a*b") → true
+*/
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(p.empty()) return s.empty();
+        if(p[1] != '*'){
+            if(s[0] != '\0' && (s[0] == p[0] || p[0] == '.')) return isMatch(s.substr(1),p.substr(1));
+            else return false;
+        }else
+        {
+            while(s[0] != '\0' && (s[0] == p[0] || p[0] == '.'))
+            {
+                if(isMatch(s,p.substr(2))) return true;
+                s.erase(s.begin());
+            }
+            return isMatch(s,p.substr(2));
+        }
+    }
+};
